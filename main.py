b92288e2de7f44e44e4c4c66e6aae7bd9085b98e
nhutrang0209 <108412886+nhutrang0209@users.noreply.github.com>
2023-07-21 15:53:37 +0700

    authentication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from flask import Flask, render_template, request, send_file, make_response, jsonify
+import urllib.parse
 import random
 import string
 import base64
@@ -9,11 +10,16 @@ import time
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a44cr1eoi1jnsoleuspocs0mr7'
+APIkey = 'hr3ma4m73f1m0bhm9kssjhk6jq'
+app.config['SECRET_KEY'] = 'hr3ma4m73f1m0bhm9kssjhk6jq'
 scan_progress = {
     "spiderProgress": 0,
     "activeScanProgress": 0
 }
+context_id = 1
+apikey = 'hr3ma4m73f1m0bhm9kssjhk6jq'
+context_name = 'Default Context'
+
 
 @app.route('/')
 def index():
@@ -61,31 +67,71 @@ def verify():
 def start_scan():
     url = request.form['url']
     status = "Scanning"
-    apiKey = 'a44cr1eoi1jnsoleuspocs0mr7'
-    zap = ZAPv2(apikey=apiKey)
+    zap = ZAPv2(apikey=APIkey)
     zap.core.new_session()
-    start_spider(url)
+    set_include_in_context()
+    set_form_based_auth()
+    set_logged_in_indicator()
+    user_id_response = set_user_auth_config()
+    start_spider(user_id_response, url)
     start_active(url)
     status = "Completed";
     return render_template('download.html', url=url, status = status)
+
+def set_include_in_context():
+    zap = ZAPv2(apikey=APIkey)
+    include_url = 'http://localhost:1337/.*'
+    zap.context.include_in_context(context_name, include_url)
+    print('Configured include and exclude regex(s) in context')
+
+
+def set_logged_in_indicator():
+    zap = ZAPv2(apikey=APIkey)
+    logged_in_regex = '\Q<font color="red">Welcome Bee</font>\E'
+    zap.authentication.set_logged_in_indicator(context_id, logged_in_regex)
+    print('Configured logged in indicator regex: ')
+
+
+def set_form_based_auth():
+    zap = ZAPv2(apikey=APIkey)
+    login_url = 'http://localhost:1337/login.php'
+    login_request_data = 'login={%username%}&password={%password%}&security_level=0&form=submit'
+    form_based_config = 'loginUrl=' + urllib.parse.quote(login_url) + '&loginRequestData=' + urllib.parse.quote(login_request_data)
+    zap.authentication.set_authentication_method(context_id, 'formBasedAuthentication', form_based_config)
+    print('Configured form based authentication')
+
+
+def set_user_auth_config():
+    user = 'Test User'
+    username = 'bee'
+    password = 'bug'
+
+    zap = ZAPv2(apikey=APIkey)
+    user_id = zap.users.new_user(context_id, user)
+    user_auth_config = 'username=' + urllib.parse.quote(username) + '&password=' + urllib.parse.quote(password)
+    zap.users.set_authentication_credentials(context_id, user_id, user_auth_config)
+    zap.users.set_user_enabled(context_id, user_id, 'true')
+    zap.forcedUser.set_forced_user(context_id, user_id)
+    zap.forcedUser.set_forced_user_mode_enabled('true')
+    print('User Auth Configured')
+    return user_id
+
 
 @app.route('/progress')
 def progress():
     global scan_progress
     return jsonify(scan_progress)
 
-def start_spider(url):
+def start_spider(user_id, url):
     target = url
-    apiKey = 'a44cr1eoi1jnsoleuspocs0mr7'
-    zap = ZAPv2(apikey=apiKey)
-    zap.core.new_session()
+    zap = ZAPv2(apikey=APIkey)
+    zap.spider.scan_as_user(context_id, user_id, target)
     scan_id = zap.spider.scan(target)
     spider_progress(scan_id,target)   
     
 #Quá trình spider
 def spider_progress(scan_id,target):
-    apiKey = 'a44cr1eoi1jnsoleuspocs0mr7'
-    zap = ZAPv2(apikey=apiKey)
+    zap = ZAPv2(apikey=APIkey)
     global scan_progress
     scan_status = zap.spider.status(scan_id) #số %
     scan_progress["spiderProgress"] = int(scan_status)
@@ -97,16 +143,14 @@ def spider_progress(scan_id,target):
 #Bắt đầu active scan
 def start_active(url):
         target = url
-        apiKey = 'a44cr1eoi1jnsoleuspocs0mr7'
-        zap = ZAPv2(apikey=apiKey)
+        zap = ZAPv2(apikey=APIkey)
         scan_id = zap.ascan.scan(target)
         active_progress(scan_id,target) 
 
 
 #Quá trình active scan
 def active_progress(scan_id,target): #Quá trình active scan
-        apiKey = 'a44cr1eoi1jnsoleuspocs0mr7'
-        zap = ZAPv2(apikey=apiKey)
+        zap = ZAPv2(apikey=APIkey)
         global scan_progress
         scan_status = zap.ascan.status(scan_id)
         scan_progress["activeScanProgress"] = int(scan_status)
@@ -122,8 +166,7 @@ def active_progress(scan_id,target): #Quá trình active scan
 @app.route('/report')
 def generate_report():
     # Tạo báo cáo HTML từ ZAP
-    apiKey = 'a44cr1eoi1jnsoleuspocs0mr7'
-    zap = ZAPv2(apikey=apiKey)
+    zap = ZAPv2(apikey=APIkey)
     report_html = zap.core.htmlreport()
     
     # Đường dẫn thư mục hiện tại (thư mục chứa file Python)
@@ -158,9 +201,9 @@ def get_domain_from_url(url):
     return domain
 
 def generate_random_name():
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
     encoded_string = base64.b64encode(random_string.encode()).decode()
-    file_name = encoded_string[:8]
+    file_name = encoded_string[:32]
     return file_name
 
 def generate_html_file(url, file_name):
